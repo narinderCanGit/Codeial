3a9eca4343c0f8ac1c580626e310fcb32970ca94
narinderCanGit <narinder111198@gmail.com>
2020-06-21 15:02:57 +0530

    Install ejs and setup view engine

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ console.log('router loaded');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));     //localhost:8000/users/profile
-router.use('/users', require('./users'));    // localhost:8000/users/posts
+//router.use('/users', require('./users'));    // localhost:8000/users/posts
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
