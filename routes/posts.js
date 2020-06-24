52caacfb1c522040fcd545aa51a72eb77d1d3a0f
narinderCanGit <narinder111198@gmail.com>
2020-06-24 22:50:01 +0530

    Deleting a post

--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,7 @@ const passport = require('passport');
 const postsController = require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthentication, postsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy); 
+// :id is the param , passed to controllers/posts_controller  
 
 module.exports = router;
