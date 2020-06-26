b5d3771445ae9eba564c9e91b6a5bad4623e7795
narinderCanGit <narinder111198@gmail.com>
2020-06-26 14:46:05 +0530

    Converted the remaining controllers to async await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,44 +1,57 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = async function(req, res){
 
-        if (post){
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            }, function(err, comment){
-                // handle error
+    try{
 
-                post.comments.push(comment);
-                post.save();
+       let post = await Post.findById(req.body.post);
 
-                res.redirect('/');
-            });
+       if (post){
+        let comment = await Comment.create({
+            content: req.body.content,
+            post: req.body.post,
+            user: req.user._id
+        });
+
+            post.comments.push(comment);
+            post.save();
+
+            res.redirect('/');
         }
 
-    });
+    }
+    catch(err){
+        console.log('error',err);
+        return;
+    }
+
 }
 
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
-        if (comment.user == req.user.id){
+module.exports.destroy = async function(req, res){
 
+    try{
+
+        let comment = await Comment.findById(req.params.id);
+        if (comment.user == req.user.id){
+    
             let postId = comment.post;    
             //postId will be used to delete comment from the comment array in the post model
 
             comment.remove();
 
-            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post){
+            let post = await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
             //$pull: {comments: req.params.id}} delete 1 comment by this id and update the post model(db)
                 return res.redirect('back');
-            })
-        }
+            }
         else{
             return res.redirect('back');
         }
-    });
+
+    }catch(err){
+        console.log('error',err);
+        return;
+    }
+    
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -49,25 +49,30 @@ module.exports.signIn = function(req, res){
 }
 
 // get the sign up data
-module.exports.create = function(req, res){
-    if (req.body.password != req.body.confirm_password){
-        return res.redirect('back');
-    }
+module.exports.create = async function(req, res){
 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
+    try{
 
-        if (!user){
-            User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
-
-                return res.redirect('/users/sign-in');
-            })
-        }else{
+        if (req.body.password != req.body.confirm_password){
             return res.redirect('back');
         }
+    
+        let user = await User.findOne({email: req.body.email});
+           
+        if (!user){
+            User.create(req.body);
+            return res.redirect('/users/sign-in');
 
-    });
+            } else{
+            return res.redirect('back');
+        }
+            
+    
+    }catch(err){
+        console.log('error',err);
+        return;
+    }
+    
 }
 
 
