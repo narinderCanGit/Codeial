d46587dfeda0a2434eb2025ebc0af0f204f488ef
narinderCanGit <narinder111198@gmail.com>
2020-06-26 13:33:43 +0530

    async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,36 +1,41 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
+module.exports.home = async function(req, res){
 
     // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users: users
-            });
-        });
-       
-    })
 
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
+       let users = await User.find({});
+
+       return res.render('home', {
+        title: "Codeial | Home",
+        posts:  posts,
+        all_users: users
+    });
+
+    }
+    catch(err){
+        console.log('error',err);
+        return;
+    }
 }
 // module.exports.actionName = function(req, res){}
+
+//promise
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+
+// posts.then()
