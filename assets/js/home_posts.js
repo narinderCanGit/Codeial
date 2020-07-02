3d36484561a3506160c7fd23c8821cfee3bd9c42
narinderCanGit <narinder111198@gmail.com>
2020-07-02 16:37:09 +0530

    Deleting a post

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -14,6 +14,7 @@
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost); 
                     //prepend is used to show our comment on the top
+                    deletePost($(' .delete-post-button', newPost));
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -29,7 +30,7 @@
                     <p>
                         
                         <small>
-                            <a class="delete-post-button"  href="/posts/destroy/${ post.id }">X</a>
+                            <a class="delete-post-button"  href="/posts/destroy/${ post._id }">X</a>
                         </small>
                        
                         ${ post.content }
@@ -56,6 +57,26 @@
                     
                 </li>`)
     }
+
+
+     // method to delete a post from DOM
+     let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                },error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+
+        });
+    }
+
     
 
     createPost();
