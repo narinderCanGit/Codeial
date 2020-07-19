c8893416c969b63a5243f788c956d5700c034d2b
narinderCanGit <narinder111198@gmail.com>
2020-07-19 16:17:22 +0530

    Action and routes for likes

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.use('/users', require('./users'));     //localhost:8000/users/profile
 //router.use('/users', require('./users'));    // localhost:8000/users/posts
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
+router.use('/likes', require('./likes'));
 
 router.use('/api', require('./api'));
 
